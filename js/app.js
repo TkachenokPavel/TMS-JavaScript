02b5c4aa7eef0200fdd388b5dd611dc6ea333372
TkachenokPavel <pasha.tkachenok@mail.ru>
2022-04-07 14:51:34 +0300

    Done HW15

--- js/app.js
+++ js/app.js
@@ -1,1 +1,115 @@
 "use strict";
+
+
+// Task 1
+// let x = 20;  
+// let y = 58;    
+// let z = 42;
+
+// let result = x + y +z;
+
+// console.log(result);
+
+
+//Task 2
+// let secondInMinute = 60;
+// let minuteInHour = 60;
+// let hourInDay = 24;
+// let dayInYear = 365;
+// let age = 26;
+
+// let ageInSeconds = age * (dayInYear * hourInDay * minuteInHour *secondInMinute);
+
+// console.log(ageInSeconds);
+
+
+// Task 3
+
+// let count = 42;
+// let userName = '42';
+
+//var1
+
+// userName = +userName;
+// count += "";
+
+// console.log(typeof(userName), typeof(count));
+
+//var2
+
+// userName = Number(userName);
+// count = String(count);
+
+// console.log(typeof(userName), typeof(count));
+
+
+//Task 4
+
+// let a = 1;
+// let b = 2;
+// let c = "белых медведей";
+
+// let total = String(a)+ String(b) + ' ' + c;
+
+// console.log(total);
+
+//Task 5 
+
+// let word1= 'доступ';
+// let word2= 'морпех';
+// let word3= 'наледь';
+// let word4= 'попрек';
+// let word5= 'рубило';
+
+// let lengthWords = word1.length + word2.length + word3.length + word4.length + word5.length;
+// console.log(lengthWords);
+
+//Task 6
+
+// let userName = "Pavel";
+// let userAge = 26;
+// let userSmoking = true;
+
+// console.log(`Variable: ${userName} have type: ${typeof(userName)}`);
+// console.log(`Variable: ${userAge} have type: ${typeof(userAge)}`);
+// console.log(`Variable: ${userSmoking} have type: ${typeof(userSmoking)}`);
+
+//Task 7
+
+let userName = prompt('Как Вас зовут?', '');
+let userAge = prompt('Сколько вам лет', '');
+
+console.log(`Name: ${userName}, Age:${userAge}`);
+
+
+// ADVANCED level
+
+// Task 1 
+
+// let a = 4;
+// let b = 3;
+
+//var1
+
+// a = a + b;
+// b = a - b ;
+// a = a - b;
+
+//var2
+
+// [a, b] = [b, a];
+
+// console.log(a, b);
+
+
+//Task 2
+
+// let codeWord1 = "обернись";
+// let codeWord2 = "неужели";
+// let codeWord3 = "огурцы";
+// let codeWord4 = "липкие";
+// let codeWord5 = "?!";
+
+// let cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
+
+// console.log(cipher);
